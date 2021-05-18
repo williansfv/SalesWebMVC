cfcef04c535dbc932486a7d79f3d326bdd0a6b6f
Willians Viana <willians.viana@outlook.com>
2021-05-18 09:24:06 -0300

    Exceptions handling for delete

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -83,8 +83,14 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
+            try { 
             await _sellerService.RemoveAsync(id);
             return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message});
+            }
 
         }
 
